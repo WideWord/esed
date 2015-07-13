94a31fe1877b85d727550fe9ac6a295b182ae082
Cyril Vilkov <wide.wrd@gmail.com>
2015-07-13 19:00:09 +0300

    args parsing

--- src/main.c
+++ src/main.c
@@ -1,9 +1,46 @@
 
 #include <stdio.h>
+#include "argparser.h"
+#include "operations.h"
+#include <stdlib.h>
 
-int main() {
 
-	printf("This is esed\n");
+
+int main(int argc, char ** argv) {
+
+	esedArgs * args = esedParseArgs(argc, argv);
+
+	if (args->command == NULL) {
+		fprintf(stderr, "Command not provided, use 'esed -h' for help\n");
+		exit(1);
+	}
+
+	FILE * in;
+	if (args->inputFile != NULL) {
+		in = fopen(args->inputFile, "r");
+	} else {
+		in = stdin;
+	}
+
+	FILE * out;
+	if (args->outputFile != NULL) {
+		out = fopen(args->outputFile, "w");
+	} else {
+		out = stdout;
+	}
+
+	esedOperation operations[] = {
+		NULL,
+		(esedOperation)esedReplace,
+		(esedOperation)esedInsertLine,
+		(esedOperation)esedRemoveLine,
+		(esedOperation)esedInsertLineNearPattern
+	};
+
+	esedOperation operation = operations[args->command->type];
+	operation(in, out, args->command);
+
+	esedFreeArgs(args); 
 
 	return 0;
 }
