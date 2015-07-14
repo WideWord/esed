852fd36f83990a3a77b98ae135f0d9d45bbb68db
GoAlexander <dydychckin@yandex.ru>
2015-07-14 14:52:13 +0300

    My implementation of remove_line_by_number. First version, it needs to be tested.

--- src/operations.c
+++ src/operations.c
@@ -10,7 +10,32 @@ void esedInsertLine(FILE * in, FILE * out, esedInsertLineCommand * cmd) {
 }
 
 void esedRemoveLine(FILE * in, FILE * out, esedRemoveLineCommand * cmd) {
-	fprintf(stderr, "Remove operation not implemented, lineNumber = '%d'\n", cmd->lineNumber);
+	//fprintf(stderr, "Remove operation not implemented, lineNumber = '%d'\n", cmd->lineNumber);
+ 	
+	char element;
+
+	//we go until extracting line
+	for (int i(0); i!=cmd->lineNumber-1; i++)
+	{
+		fgets (in, "%c", element);
+		for (int number_of_element(0); element!='\n'; number_of_element++)
+			fprintf (out, "%c",element);
+			fgets (in, "%c", element);
+		fprintf (out, "\n");
+	}
+
+	//we "miss" extracting line
+	fgets (in, "%c", element);
+	for (int number_of_element(0); element!='\n'; number_of_element++)
+		fgets (in, "%c", element);
+
+	//we go until EOF
+	fgets (in, "%c", element);
+	while (element!=EOF)
+	{
+		fprintf (out, "%c", element);
+		fgets (in, "%c", element);
+	}
 }
 
 void esedInsertLineNearPattern(FILE * in, FILE * out, esedInsertLineNearPatternCommand * cmd) {
