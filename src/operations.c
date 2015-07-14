ba8222b98ba4a7b0cdb6342df374cf38dd0db7d7
TetoFag-kun <aleksey.glebow@gmail.com>
2015-07-14 22:39:50 +0300

    started creating my function, unworking now

--- src/operations.c
+++ src/operations.c
@@ -39,5 +39,54 @@ void esedRemoveLine(FILE * in, FILE * out, esedRemoveLineCommand * cmd) {
 }
 
 void esedInsertLineNearPattern(FILE * in, FILE * out, esedInsertLineNearPatternCommand * cmd) {
-	fprintf(stderr, "Insert near pattern operation not implemented, pattern = '%s', string = '%s', below = '%d'\n", cmd->pattern, cmd->string, cmd->below);
+	char str[50];
+	int sym = 0;
+	fgets(str,sizeof(str),in);
+	int patsym = 0;
+	int status = 0;
+	int l = strlen(str);
+	int i = strlen(cmd->pattern);
+	if(str == NULL){
+		printf("1 \n");
+	}
+	fgets(str,sizeof(str),in);
+	if(str == NULL){
+		printf("1 \n");
+	}
+	printf("%d \n", i);
+	while(str != NULL){
+		fgets(str,sizeof(str),in);
+		//if(strlen(str - 1 != strlen(cmd->pattern)){
+		//		status = 0;
+		//	}
+		//else
+		 while(str[sym] != '\n'){
+			if(cmd->pattern[patsym]==str[sym]){
+				status = 1;
+				sym++;
+				patsym++;
+			}
+			else if(cmd->pattern[patsym] == '?'){
+				sym++;
+				patsym++;
+			}
+			//else if(cmd->pattern[patsym] == '*'){
+			//	while(cmd->pattern[patsym] != currentStr[sym]){
+
+			//	}
+		}
+		if(status == 1){
+			if(cmd->below == 0){
+				fputs(cmd->string, out);
+				fputs(str, out);
+			}
+			else{
+				fputs(str, out);
+				fputs(cmd->string, out);
+			}
+		}
+		else {fputs(str, out);}
+		fgets(str,sizeof(str),in);
+	}
+
 }
